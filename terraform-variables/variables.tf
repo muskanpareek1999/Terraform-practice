227c19baa8de015c85692b1bb0da448e767e5bf5
Muskan PareeK <42691705+muskanpareek1999@users.noreply.github.com>
2023-07-18 17:30:41 +0530

    Create variables.tf

---
+++ terraform-variables/variables.tf
@@ -0,0 +1,8 @@
+variable "filename" {
+	default = "/home/ubuntu/terraform/day2/terraform-variables/devops-automated.txt"
+}
+
+variable "content" {
+	default = "This is auto generated from a variable"
+}
+variable "devops_op_trainer" {}
